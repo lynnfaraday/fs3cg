a1611d5a4bd5ea62624741470413f655a9c58c35
lynnfaraday <lynn@wordsmyth.org>
2016-07-16 13:50:05 -0400

    Making action skills dynamic.

--- app/controllers/chargen.js
+++ app/controllers/chargen.js
@@ -12,6 +12,7 @@ export default Ember.Controller.extend({
     fs3action: null,
     selectedTheme: '100',
     attrErrors: [],
+    skillErrors: [],
     
     init: function() {
         this.notifications.setDefaultAutoClear(true);
@@ -29,46 +30,74 @@ export default Ember.Controller.extend({
         ];
         this.set('fs3attrs', attrs);
         
-        let skills = Ember.Object.create({ archery: 1, athletics: 1, firearms: 1, medicine: 1, melee: 1, tinkering: 1, stealth: 1, thrown: 1, gunnery: 1, piloting: 1, demolitions: 1, deception: 1, persuasion: 1, riding: 1, survival: 1, tactics: 1, scrounge: 1});
+        let skills = [];
+        if (this.get('selectedTheme') === "100") {
+            skills = [
+            Ember.Object.create( { name: "Archery", desc: "Shooting bows and crossbows. (Reflexes)", rating: 1 }),
+            Ember.Object.create( { name: "Athletics", desc: "General running, jumping, climbing, etc. (Brawn)", rating: 1 }),
+            Ember.Object.create( { name: "Deception", desc: "Lies and cons. (Presence)", rating: 1 }),
+            Ember.Object.create( { name: "Firearms", desc: "Shooting guns. (Reflexes)", rating: 1 }),
+            Ember.Object.create( { name: "Medicine", desc: "Tending to the ill and injured. (Wits)", rating: 1 }),
+            Ember.Object.create( { name: "Melee", desc: "Fighting with fists, knives, and hand-to-hand weapons. (Brawn)", rating: 1 }),
+            Ember.Object.create( { name: "Persuasion", desc: "Making friends and influencing people. (Presence)", rating: 1 }),
+            Ember.Object.create( { name: "Riding", desc: "Riding horses. (Brawn)", rating: 1 }),
+            Ember.Object.create( { name: "Scrounge", desc: "Knowing where to look to find useful stuff in the ruins. (Wits)", rating: 1 }),
+            Ember.Object.create( { name: "Stealth", desc: "Being sneaky. (Reflexes)", rating: 1 }),
+            Ember.Object.create( { name: "Survival", desc: "Surviving in the wilderness, including trapping/foraging/shelters/navigation. (Wits)", rating: 1 }),
+            Ember.Object.create( { name: "Tactics", desc: "Warfare and tactics. (Wits)", rating: 1 }),
+            Ember.Object.create( { name: "Tinkering", desc: "Mechanical/electrical tinkering and repair. (Wits)", rating: 1 }),
+            Ember.Object.create( { name: "Thrown", desc: "Thrown weapons. (Reflexes)", rating: 1 }),
+            ];
+        }
+        else if (this.get('selectedTheme') == "Battlestar Galactica") {
+            
+            skills = [
+            Ember.Object.create( { name: "Athletics", desc: "General running, jumping, climbing, etc. (Brawn)", rating: 1 }),
+            Ember.Object.create( { name: "Demolitions", desc: "Blowing stuff up. (Wits)", rating: 1 }),
+            Ember.Object.create( { name: "Firearms", desc: "Shooting guns. (Reflexes)", rating: 1 }),
+            Ember.Object.create( { name: "Medicine", desc: "Tending to the ill and injured. (Wits)", rating: 1 }),
+            Ember.Object.create( { name: "Gunnery", desc: "Vehicle and heavy weapons. (Reflexes)", rating: 1 }),
+            Ember.Object.create( { name: "Melee", desc: "Fighting with fists, knives, and hand-to-hand weapons. (Brawn)", rating: 1 }),
+            Ember.Object.create( { name: "Piloting", desc: "Flying Vipers, Raptors and airborne vehicles. (Reflexes)", rating: 1 }),
+            Ember.Object.create( { name: "Stealth", desc: "Being sneaky. (Reflexes)", rating: 1 }),
+            Ember.Object.create( { name: "Tinkering", desc: "Mechanical/electrical tinkering and repair. (Wits)", rating: 1 })
+            ];           
+        }
+        else {
+            skills = [
+            Ember.Object.create( { name: "Archery", desc: "Shooting bows and crossbows. (Reflexes)", rating: 1 }),
+            Ember.Object.create( { name: "Athletics", desc: "General running, jumping, climbing, etc. (Brawn)", rating: 1 }),
+            Ember.Object.create( { name: "Demolitions", desc: "Blowing stuff up. (Wits)", rating: 1 }),
+            Ember.Object.create( { name: "Firearms", desc: "Shooting guns. (Reflexes)", rating: 1 }),
+            Ember.Object.create( { name: "Gunnery", desc: "Vehicle and heavy weapons. (Reflexes)", rating: 1 }),
+            Ember.Object.create( { name: "Medicine", desc: "Tending to the ill and injured. (Wits)", rating: 1 }),
+            Ember.Object.create( { name: "Melee", desc: "Fighting with fists, knives, and hand-to-hand weapons. (Brawn)", rating: 1 }),
+            Ember.Object.create( { name: "Riding", desc: "Riding horses. (Brawn)", rating: 1 }),
+            Ember.Object.create( { name: "Scrounge", desc: "Knowing where to look to find useful stuff in the ruins. (Wits)", rating: 1 }),
+            Ember.Object.create( { name: "Stealth", desc: "Being sneaky. (Reflexes)", rating: 1 }),
+            Ember.Object.create( { name: "Survival", desc: "Surviving in the wilderness, including trapping/foraging/shelters/navigation. (Wits)", rating: 1 }),
+            Ember.Object.create( { name: "Tinkering", desc: "Mechanical/electrical tinkering and repair. (Wits)", rating: 1 })
+            ];
+        }
+        
+            Ember.Object.create({ archery: 1, athletics: 1, firearms: 1, medicine: 1, melee: 1, tinkering: 1, stealth: 1, thrown: 1, gunnery: 1, piloting: 1, demolitions: 1, deception: 1, persuasion: 1, riding: 1, survival: 1, tactics: 1, scrounge: 1});
         this.set('fs3action', skills);
     }.observes('selectedTheme'),
     
-    showLowTech: function() {
-        switch (this.get('selectedTheme')) {
-        case '100':
-            return true;
-        case 'Modern Post-Apocalyptic':
-            return true;
-        default:
-            return false;
-        }
-    }.property('selectedTheme'),
-    
-    showHighTech: function() {
-        switch (this.get('selectedTheme')) {
-        case '100':
-            return false;
-        default:
-            return true;
-        }
-    }.property('selectedTheme'),
-
-    show100: function() {
-        switch (this.get('selectedTheme')) {
-        case '100':
-            return true;
-        default:
-            return false;
-        }
-    }.property('selectedTheme'),
-
     showAttrErrors: function() {
         if (this.get('attrErrors').length > 0) {
             return true;
         }
         return false;
     }.property('attrErrors'),
-    
+
+    showSkillErrors: function() {
+        if (this.get('skillErrors').length > 0) {
+            return true;
+        }
+        return false;
+    }.property('skillErrors'),
+        
     attrPoints: function() {
         let totalAttrs = 0;
         let fa = this.get('fs3attrs');
@@ -99,12 +128,17 @@ export default Ember.Controller.extend({
             this.set('skillErrors', []);
             
             let highAttrs = this.countHigh(this.get('fs3attrs'), 5);
-            let highSkills = this.countHigh(this.get('fs3attrs'), 5);
+            let highSkills = this.countHigh(this.get('fs3action'), 5);
             
             if (highAttrs > 1)
             {
                 //this.notifications.error('You can only have one attribute at 5.');
                 this.attrErrors.push('You can only have one attribute at 5.  If you think this limit is bad, please note it in the comments.');
+            }
+            
+            if (highSkills > 3)
+            {
+                this.skillErrors.push('You can only have 3 skills at 5+.  If you think this limit is bad, please note it in the comments.');
             }
             
             let totalAttrs = this.attrPoints();
